baa2c3a530784559164be4e69be1486f4b43af26
Vladislav Shyiatyi <shiatiy.vlad@gmail.com>
2021-03-19 02:25:29 +0200

    Minor fixes

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import '../js/filter.js'
+import '../js/film.js'
+import '../js/fetchData.js'
+import '../js/favorite.js'
+import '../js/createElement.js'
+import '../js/constants.js'
+import '../js/pagination.js'
+import '../js/main.js'
